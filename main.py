f68591c0a0023c176f76332d98d35386056fad0a
NyaShinn1204 <team.nyarara@gmail.com>
2024-01-20 22:34:08 +0900

    Update Theme, Copy of Raider created in the past

--- main.py
+++ main.py
@@ -79,6 +79,7 @@ c7 = "#000117"
 c8 = "#489ea1"
 c9 = "#454c7f"
 c10 = "#2D2DA0"
+c11 = "#041432"
 
 root = tk.Tk()
 root.geometry("1280x720")
@@ -122,19 +123,88 @@ def module_scroll_frame(num1, num2):
         
   if num1 == 2:
     if num2 == 1:
+      modules_frame10_01 = ctk.CTkFrame(module_frame, width=470, height=210, border_width=0, fg_color=c11)
+      modules_frame10_01.grid(row=0, column=0, padx=6, pady=6)
+      tk.Label(modules_frame10_01, bg=c11, fg="#fff", text="Tokens", font=("Roboto", 14)).place(x=15,y=0)
+      tk.Canvas(modules_frame10_01, bg=c6, highlightthickness=0, height=4, width=470).place(x=0, y=25)
+      ctk.CTkButton(modules_frame10_01, text="Select File", fg_color=c2, hover_color=c5, width=75, height=25).place(x=5,y=33)
+      ctk.CTkEntry(modules_frame10_01, bg_color=c11, fg_color=c7, border_color=c4, text_color="#fff", width=150, height=20, state="disabled").place(x=85,y=33)
+      ctk.CTkLabel(modules_frame10_01, bg_color=c11, fg_color=c4, text_color="#fff", text="", width=150, height=20).place(x=85,y=33)
+      tk.Label(modules_frame10_01, bg=c11, fg="#fff", text="File Name", font=("Roboto", 12)).place(x=240,y=31)
+
+      tk.Label(modules_frame10_01, bg=c11, fg="#fff", text="Status", font=("Roboto", 12)).place(x=5,y=70)
+      tk.Label(modules_frame10_01, bg=c11, fg="#fff", text="Total: 000", font=("Roboto", 12)).place(x=10,y=95)
+      tk.Label(modules_frame10_01, bg=c11, fg="#fff", text="Valid: 000", font=("Roboto", 12)).place(x=10,y=115)
+      tk.Label(modules_frame10_01, bg=c11, fg="#fff", text="Invalid: 000", font=("Roboto", 12)).place(x=10,y=135)
+      
+      modules_frame10_02 = ctk.CTkFrame(module_frame, width=470, height=210, border_width=0, fg_color=c11)
+      modules_frame10_02.grid(row=0, column=1, padx=6, pady=6)
+      tk.Label(modules_frame10_02, bg=c11, fg="#fff", text="Proxies", font=("Roboto", 14)).place(x=15,y=0)
+      tk.Canvas(modules_frame10_02, bg=c6, highlightthickness=0, height=4, width=470).place(x=0, y=25)
+      ctk.CTkCheckBox(modules_frame10_02, bg_color=c11, text_color="#fff", border_color=c3, checkbox_width=20, checkbox_height=20, hover=False, border_width=3 ,text="Enabled").place(x=5,y=31)
+      ctk.CTkOptionMenu(modules_frame10_02, values=["http", "https", "socks4", "socks5"], fg_color=c7, button_color=c5, button_hover_color=c4).place(x=5,y=57)
+      tk.Label(modules_frame10_02, bg=c11, fg="#fff", text="Socket Type", font=("Roboto", 12)).place(x=150,y=55)
+      ctk.CTkButton(modules_frame10_02, text="Select File", fg_color=c2, hover_color=c5, width=75, height=25).place(x=5,y=90)
+      ctk.CTkEntry(modules_frame10_02, bg_color=c11, fg_color=c7, border_color=c4, text_color="#fff", width=150, height=20, state="disabled").place(x=85,y=90)
+      ctk.CTkLabel(modules_frame10_02, bg_color=c11, fg_color=c4, text_color="#fff", text="", width=150, height=20).place(x=85,y=90)
+      tk.Label(modules_frame10_02, bg=c11, fg="#fff", text="File Name", font=("Roboto", 12)).place(x=240,y=87)
+    
+      tk.Label(modules_frame10_02, bg=c11, fg="#fff", text="Status", font=("Roboto", 12)).place(x=5,y=120)
+      tk.Label(modules_frame10_02, bg=c11, fg="#fff", text="Total: 000", font=("Roboto", 12)).place(x=10,y=145)
+      tk.Label(modules_frame10_02, bg=c11, fg="#fff", text="Valid: 000", font=("Roboto", 12)).place(x=10,y=165)
+      tk.Label(modules_frame10_02, bg=c11, fg="#fff", text="Invalid: 000", font=("Roboto", 12)).place(x=10,y=185)
+   
+      modules_frame10_03 = ctk.CTkFrame(module_frame, width=470, height=200, border_width=0, fg_color=c11)
+      modules_frame10_03.grid(row=1, column=0, padx=6, pady=6)
+      tk.Label(modules_frame10_03, bg=c11, fg="#fff", text="Settings", font=("Roboto", 14)).place(x=15,y=0)
+      tk.Canvas(modules_frame10_03, bg=c6, highlightthickness=0, height=4, width=470).place(x=0, y=25)
+      
+      CTkLabel(modules_frame10_03, text_color="#fff", text="Default Delay Time (s)", font=("Roboto", 15)).place(x=5,y=30)
+      test = ctk.CTkSlider(modules_frame10_03, from_=0.1, to=3.0)
+      test.place(x=5,y=55)
+      
+      CTkLabel(modules_frame10_03, text_color="#fff", text="Default Mention Count (m)", font=("Roboto", 15)).place(x=5,y=79)
+      test = ctk.CTkSlider(modules_frame10_03, from_=1, to=50)
+      test.place(x=5,y=104)
+      
+      ctk.CTkButton(modules_frame10_03, text="Get Info     ", fg_color=c2, hover_color=c5, width=75, height=25).place(x=5,y=126)
+      invite_url = ctk.CTkEntry(modules_frame10_03, bg_color=c11, fg_color=c7, border_color=c4, text_color="#fff", width=150, height=20)
+      invite_url.place(x=85,y=126)
+      tk.Label(modules_frame10_03, bg=c11, fg="#fff", text="Defalut Server ID", font=("Roboto", 12)).place(x=240,y=124)
+
+      modules_frame10_04 = ctk.CTkFrame(module_frame, width=470, height=200, border_width=0, fg_color=c11)
+      modules_frame10_04.grid(row=1, column=1, padx=6, pady=6)
+      tk.Label(modules_frame10_04, bg=c11, fg="#fff", text="Custom Theme", font=("Roboto", 14)).place(x=15,y=0)
+      tk.Canvas(modules_frame10_04, bg=c6, highlightthickness=0, height=4, width=470).place(x=0, y=25)
+      tk.Label(modules_frame10_04, bg=c11, fg="#fff", text="Theme Select", font=("Roboto", 12)).place(x=5,y=30)
+      def combobox_callback(choice):
+        global theme
+        printl("info", "Select Theme " + choice)
+        if choice == "Default Theme":
+          theme = "twocoin"
+        if choice == "Akebi Theme":
+          theme = "akebi"
+        print(theme)
+        with open('config.json') as f:
+          data = json.load(f)
+        data.update({"select_theme": theme})
+        with open('config.json', 'w') as f:
+          json.dump(data, f)
+  
+      ctk.CTkOptionMenu(master=modules_frame10_04, values=["Default Theme", "Akebi Theme"], fg_color=c7, button_color=c5, button_hover_color=c4, text_color="#fff", command=combobox_callback).place(x=5, y=55)
 
       printl("debug", "Open Setting Tab")
         
     if num2 == 2:
       credits_frame = ctk.CTkFrame(module_frame, width=940, height=400, border_width=0, fg_color=c2)
       credits_frame.grid(row=1, column=1, padx=6, pady=6)
-      tk.Label(credits_frame, bg=c2, fg=c8, text="ThreeCoin github:", font=("Roboto", 12)).place(x=0,y=0)
+      tk.Label(credits_frame, bg=c2, fg=c8, text="ThreeCoinRaider github:", font=("Roboto", 12)).place(x=0,y=0)
       test = tk.Label(credits_frame, bg=c2, fg=c9, text="Github link", font=("Roboto", 12, "underline"))
-      test.place(x=135,y=0)
+      test.place(x=175,y=0)
       test.bind("<Button-1>", lambda e:webbrowser.open_new("https://github.com/NyaShinn1204/ThreeCoinRaider"))
-      tk.Label(credits_frame, bg=c2, fg=c8, text="ThreeCoin discord:", font=("Roboto", 12)).place(x=0,y=25)
+      tk.Label(credits_frame, bg=c2, fg=c8, text="ThreeCoinRaider discord:", font=("Roboto", 12)).place(x=0,y=25)
       test = tk.Label(credits_frame, bg=c2, fg=c9, text="Discord invite link", font=("Roboto", 12, "underline"))
-      test.place(x=140,y=25)
+      test.place(x=180,y=25)
       test.bind("<Button-1>", lambda e:webbrowser.open_new("https://discord.gg/4AZNXaCVHv"))
       tk.Label(credits_frame, bg=c2, fg="#fff", text="Main developer and updater:", font=("Roboto", 12)).place(x=0,y=50)
       tk.Label(credits_frame, bg=c2, fg=c10, text=developer, font=("Roboto", 12)).place(x=210,y=50)
