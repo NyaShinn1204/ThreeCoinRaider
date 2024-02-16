6890f2559bdc62354f9729e216e78bb2e5c1ab14
NyaShinn1204 <134133778+NyaShinn1204@users.noreply.github.com>
2024-02-16 11:26:14 +0900

    Update get_session

--- bypass/get_session.py
+++ bypass/get_session.py
@@ -2,7 +2,7 @@ import tls_client
 
 def get_session():
     session = tls_client.Session(
-        client_identifier="chrome_105",
+        client_identifier="chrome_120",
         random_tls_extension_order=True,
         ja3_string="771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,10-23-27-43-13-65281-16-5-45-18-0-11-35-17513-51-21-41,29-23-24,0",
         h2_settings={"HEADER_TABLE_SIZE": 65536,"MAX_CONCURRENT_STREAMS": 1000,"INITIAL_WINDOW_SIZE": 6291456,"MAX_HEADER_LIST_SIZE": 262144},
